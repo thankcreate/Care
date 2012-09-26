7c68bc08a1af7473c542019c74a43e930f701fc8
thankcreate <thankcreate@gmail.com>
2012-09-26 23:55:05 +0800

    loopselector

---
+++ Care/Views/SelectOnly.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Care;
+namespace Care.Views
+{
+    public partial class SelectOnly : PhoneApplicationPage
+    {
+        public SelectOnly()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(PageLoaded);
+        }
+
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {       
+            selector.DataSource = new Care.Views.IntLoopingDataSource() { MinValue = 1, MaxValue = 60, SelectedItem = 1 };
+        }
+    }
+}
