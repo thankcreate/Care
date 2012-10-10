c817d0dd3e07d68dba7d2aadd9c87dd51b77fe2c
thankcreate <thankcreate@gmail.com>
2012-10-10 09:44:58 +0800

    model

---
+++ Care/DataModel/Renren/RenrenNews.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Care
+{
+    public class RenrenNews
+    {
+        public string post_id { get; set; }
+        public string source_id { get; set; }
+        public string feed_type { get; set; }
+        public string update_time { get; set; }
+        public string actor_id { get; set; }
+
+
+        public string name { get; set; }
+        public string headurl { get; set; }
+        public string title { get; set; }
+        public string href { get; set; }
+        public string description { get; set; }
+
+        // 这玩意儿相当于ItemViewModel里的content
+        public string message { get; set; }
+
+        // public string attachment { get; set; }
+        //public string content { get; set; }
+        //public string feed_type { get; set; }
+        //public string update_time { get; set; }
+        //public string actor_id { get; set; }
+    }
+}
---
+++ Care/DataModel/Renren/RenrenSearchedMan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Care
+{
+    public class RenrenSearchedMan
+    {
+        public string id { get; set; }
+        public string tinyurl { get; set; }
+        public string isFriend { get; set; }
+        public string name { get; set; }
+        public string info { get; set; }
+    }
+}
